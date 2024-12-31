20e2cdf59ef2b38cda0331d48e3d30f169f20d53
ShuangWu121 <shuang.wu@posteo.net>
2024-12-31 10:49:26 +0100

    multi prover compnents with same length prove nicely

--- crates/prover/src/examples/inclremental_one_multi/mod.rs
+++ crates/prover/src/examples/inclremental_one_multi/mod.rs
@@ -1,9 +1,12 @@
-use itertools::Itertools;
+
 use num_traits::One;
 
 use crate::constraint_framework::{
-    assert_constraints, EvalAtRow, FrameworkComponent, FrameworkEval, TraceLocationAllocator,
+    EvalAtRow, FrameworkComponent, FrameworkEval, TraceLocationAllocator, assert_constraints,
 };
+
+use crate::core::air::ComponentProver;
+use crate::core::air::Component;
 use crate::core::backend::simd::column::BaseColumn;
 use crate::core::backend::simd::m31::LOG_N_LANES;
 use crate::core::backend::simd::SimdBackend;
@@ -13,9 +16,15 @@ use crate::core::fields::qm31::SecureField;
 use crate::core::pcs::{CommitmentSchemeProver, PcsConfig};
 use crate::core::poly::circle::{CanonicCoset, CircleEvaluation, PolyOps};
 use crate::core::poly::BitReversedOrder;
-use crate::core::prover::{prove, StarkProof};
-use crate::core::vcs::blake2_merkle::{Blake2sMerkleChannel, Blake2sMerkleHasher};
+use crate::core::prover::prove;
+use crate::core::vcs::blake2_merkle::Blake2sMerkleChannel;
 use crate::core::ColumnVec;
+use crate::core::pcs::CommitmentSchemeVerifier;
+use itertools::chain;
+use itertools::Itertools;
+
+use crate::core::prover::verify;
+
 
 pub type IncrementalOneComponent = FrameworkComponent<IncrementalOneEval>;
 
@@ -47,29 +56,16 @@ pub struct IncrementalOneCircuitTrace {
 }
 pub fn gen_trace(
     log_size: u32,
-    circuit: &IncrementalOneCircuitTrace,
 ) -> ColumnVec<CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>> {
     let domain = CanonicCoset::new(log_size).circle_domain();
-    [&circuit.a, &circuit.b]
-        .into_iter()
-        .map(|eval| CircleEvaluation::new(domain, eval.clone()))
-        .collect()
-}
 
-#[allow(unused)]
-pub fn prove_incremental_one(
-    log_n_rows: u32,
-    config: PcsConfig,
-) -> (IncrementalOneComponent, StarkProof<Blake2sMerkleHasher>) {
-    assert!(log_n_rows >= LOG_N_LANES);
-
-    let range = 0..(1 << log_n_rows);
+    let range = 0..(1 << log_size);
     // Generate `a` and `b` as mutable vectors first
     let mut a: Vec<_> = range.clone().map(|i| i.into()).collect(); 
     let mut b: Vec<_> = range
         .clone()
         .map(|i| {
-            if i == (1 << log_n_rows) - 1 {
+            if i == (1 << log_size) - 1 {
                 0.into() // Last element is 0
             } else {
                 (i + 1).into() // Increment for all other elements
@@ -83,12 +79,27 @@ pub fn prove_incremental_one(
     crate::core::utils::bit_reverse_coset_to_circle_domain_order(&mut b);
 
     // Assign reordered `a` and `b` to the struct
-    let mut circuit = IncrementalOneCircuitTrace {
+    let circuit = IncrementalOneCircuitTrace {
         a: a.iter().map(|&i: &i32| i.into()).collect(),
         b: b.iter().map(|&i: &i32| i.into()).collect(),
     };
     println!("trace a is {:?}", circuit.a);
     println!("trace b is {:?}", circuit.b);
+
+    [&circuit.a, &circuit.b]
+        .into_iter()
+        .map(|eval| CircleEvaluation::new(domain, eval.clone()))
+        .collect()
+}
+
+#[allow(unused)]
+pub fn prove_verify_incremental_one<'a>(
+    log_n_rows: u32,
+    config: PcsConfig,
+) {
+    assert!(log_n_rows >= LOG_N_LANES);
+
+    
 
     // Precompute twiddles
     let twiddles = SimdBackend::precompute_twiddles(
@@ -96,31 +107,50 @@ pub fn prove_incremental_one(
             .circle_domain()
             .half_coset,
     );
-    println!("twiddles is {:?}", twiddles.root_coset);
+    
 
     // Setup protocol.
     let channel = &mut Blake2sChannel::default();
     let mut commitment_scheme =
         CommitmentSchemeProver::<_, Blake2sMerkleChannel>::new(config, &twiddles);
 
+    // Preprocessed columns.
+
     let mut tree_builder = commitment_scheme.tree_builder();
     let constants_trace_location = tree_builder.extend_evals([]);
     tree_builder.commit(channel);
 
     // Trace.
-    let trace = gen_trace(log_n_rows, &circuit);
+    let trace_0 = gen_trace(log_n_rows);
+    let trace_1 = gen_trace(log_n_rows);
+
+    println!("\n trace_0 is {:?} \n", trace_0);
+    println!("\n trace_1 is {:?} \n", trace_1);
+
+    let trace = chain![trace_0.clone(), trace_1.clone()].collect_vec();
+
     let mut tree_builder = commitment_scheme.tree_builder();
     let base_trace_location = tree_builder.extend_evals(trace);
     tree_builder.commit(channel);
 
-    // Prove constraints.
-    let component = IncrementalOneComponent::new(
+    // create the first component
+    let component0 = IncrementalOneComponent::new(
         &mut TraceLocationAllocator::default(),
         IncrementalOneEval { log_n_rows },
         (SecureField::one(), None),
     );
 
-    // Sanity check. Remove for production.
+    let component1 = IncrementalOneComponent::new(
+        &mut TraceLocationAllocator::default(),
+        IncrementalOneEval { log_n_rows:log_n_rows },
+        (SecureField::one(), None),
+    );
+
+    let components = &[&component0 as &dyn ComponentProver<SimdBackend>, &component1 as &dyn ComponentProver<SimdBackend>];
+
+    let components_verifier = &[&component0 as &dyn Component, &component1 as &dyn Component];
+
+    //Sanity check. Remove for production.
     let trace_polys = commitment_scheme
         .trees
         .as_ref()
@@ -129,33 +159,47 @@ pub fn prove_incremental_one(
         &trace_polys,
         CanonicCoset::new(log_n_rows),
         |mut eval| {
-            component.evaluate(eval);
+            component0.evaluate(eval);
         },
         (SecureField::one(), None),
     );
 
-    let proof = prove(&[&component], channel, commitment_scheme).unwrap();
+    println!("\n starting to generate proof.................. \n");
+    println!("log_n_rows is {:?}", log_n_rows);
 
-    (component, proof)
+    let proof = prove(components, channel, commitment_scheme).unwrap();
+
+    println!("\n proof is {:?}", proof);
+
+    // Verify.
+        // TODO: Create Air instance independently.
+        let channel = &mut Blake2sChannel::default();
+        let commitment_scheme_verifier = &mut CommitmentSchemeVerifier::<Blake2sMerkleChannel>::new(config);
+
+        // Decommit.
+        // Retrieve the expected column sizes in each commitment interaction, from the AIR.
+        // Preprocessed columns.
+        commitment_scheme_verifier.commit(proof.commitments[0], &[], channel);
+
+        // Trace columns.
+        commitment_scheme_verifier.commit(proof.commitments[1], &[log_n_rows,log_n_rows,log_n_rows,log_n_rows], channel);
+
+        verify(components_verifier, channel, commitment_scheme_verifier, proof).unwrap();
+
 }
 
 #[cfg(test)]
 mod tests {
     use std::env;
-
-    use crate::core::air::Component;
-    use crate::core::channel::Blake2sChannel;
     use crate::core::fri::FriConfig;
-    use crate::core::pcs::{CommitmentSchemeVerifier, PcsConfig};
-    use crate::core::prover::verify;
-    use crate::core::vcs::blake2_merkle::Blake2sMerkleChannel;
-    use crate::examples::incremental_one::prove_incremental_one;
+    use crate::core::pcs::PcsConfig;
+    use crate::examples::inclremental_one_multi::prove_verify_incremental_one;
 
     #[test]
-    fn test_simd_plonk_prove() {
+    fn test_simd_prove_multi() {
         // Get from environment variable:
         let log_n_instances = env::var("LOG_N_INSTANCES")
-            .unwrap_or_else(|_| "4".to_string())
+            .unwrap_or_else(|_| "6".to_string())
             .parse::<u32>()
             .unwrap();
         let config = PcsConfig {
@@ -167,23 +211,8 @@ mod tests {
             log_n_instances
         );
         // Prove.
-        let (component, proof) = prove_incremental_one(log_n_instances, config);
+        prove_verify_incremental_one(log_n_instances, config);
 
-        // Verify.
-        // TODO: Create Air instance independently.
-        let channel = &mut Blake2sChannel::default();
-        let commitment_scheme = &mut CommitmentSchemeVerifier::<Blake2sMerkleChannel>::new(config);
-
-        // Decommit.
-        // Retrieve the expected column sizes in each commitment interaction, from the AIR.
-        let sizes = component.trace_log_degree_bounds();
-
-        // Preprocessed columns.
-        commitment_scheme.commit(proof.commitments[0], &sizes[0], channel);
-
-        // Trace columns.
-        commitment_scheme.commit(proof.commitments[1], &sizes[1], channel);
-
-        verify(&[&component], channel, commitment_scheme, proof).unwrap();
+        
     }
 }
